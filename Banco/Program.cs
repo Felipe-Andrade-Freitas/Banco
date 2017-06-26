c51d72150f50e056ac41f4cf2cf6be150aa8ea6b
FelipeAndrade448 <felipeandrade448@gmail.com>
2017-06-26 14:38:00 -0300

    .

--- Banco/Program.cs
+++ Banco/Program.cs
@@ -18,6 +18,7 @@ namespace Banco
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Menu());
+            //222
         }
     }
 }
